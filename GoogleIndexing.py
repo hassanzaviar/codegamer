8c2d092a2e10b24a321e3461dbce0d30c9caabe7
Crypto Apps <135818039+hassanzaviar@users.noreply.github.com>
2024-12-31 19:50:39 +0500

    Update GoogleIndexing.py

--- GoogleIndexing.py
+++ GoogleIndexing.py
@@ -1,24 +1,23 @@
-from google.oauth2.service_account import Credentials
-from google.auth.transport.requests import Request
-from google_auth_httplib2 import AuthorizedHttp
-import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+import httplib2
 import json
+import pandas as pd
 import time
 
 # Replace with your JSON key file
 JSON_KEY_FILE = "json_key_file_downloaded_after_creating_your_google_service_account_see_above_details_on_how_to_do.json"
 SCOPES = ["https://www.googleapis.com/auth/indexing"]
 
-# Authenticate with Google Service Account
-credentials = Credentials.from_service_account_file(JSON_KEY_FILE, scopes=SCOPES)
-http = AuthorizedHttp(credentials)
+# Authenticate
+credentials = ServiceAccountCredentials.from_json_keyfile_name(JSON_KEY_FILE, scopes=SCOPES)
+http = credentials.authorize(httplib2.Http())
 
 def indexURL(url, http):
     ENDPOINT = "https://indexing.googleapis.com/v3/urlNotifications:publish"
     content = {'url': url.strip(), 'type': "URL_UPDATED"}
     json_ctn = json.dumps(content)
 
-    response, content = http.request(ENDPOINT, method="POST", body=json_ctn, headers={"Content-Type": "application/json"})
+    response, content = http.request(ENDPOINT, method="POST", body=json_ctn)
     result = json.loads(content.decode())
 
     if "error" in result:
